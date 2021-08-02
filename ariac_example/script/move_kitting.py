2b6537b45b77aee4c3e559c7ee6855d2d95c0b26
ariac <ariac@ubuntu1804.linuxvmimages.local>
2021-08-02 18:13:13 -0400

    tf frames not printing

--- ariac_example/script/move_kitting.py
+++ ariac_example/script/move_kitting.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python
 
 import rospy
+import tf2_ros
+import geometry_msgs.msg
 
 if __name__ == '__main__':
+	rospy.init_node('move_kitting')
+	
+	tfBuffer = tf2_ros.Buffer()
+	listener = tf2_ros.TransformListener(tfBuffer)
 
+	frame = 'gantry_arm_vacuum_gripper_link'
+	rate = rospy.Rate(1.0)
+	while not rospy.is_shutdown():
+		print('here')
+		try:
+			trans = tfBuffer.lookup_transform('world', frame, rospy.Time(), rospy.Duration(1.0))
+		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+			print(e)
+			continue
+
+	# Transform the pose from the specified frame to the world frame
+	local_pose = geometry_msgs.msg.PoseStamped()
+	local_pose.header.frame_id = frame
+	local_pose.pose.position.x = 0.15
+	local_pose.pose.position.y = 0.15
+
+	world_pose = tfBuffer.transform(local_pose, 'world')
+	print(world_pose)
+	rate.sleep()
+
+'''
 	kitting_group_names = ['kitting_arm']
 	#moveit_runner_kitting = MoveitRunner(kitting_group_names, ns='/ariac/kitting')
 
@@ -19,4 +46,6 @@ if __name__ == '__main__':
 		
 	z = input("Enter the desired z-value for the kitting robot: ")
 	print("Moving to (%s, %s, %s)" % (x, y, z))
+'''
+
 
