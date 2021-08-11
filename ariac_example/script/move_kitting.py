0bc054177b992c7ec39ce5f580c56d443310d16b
ariac <ariac@ubuntu1804.linuxvmimages.local>
2021-08-11 14:14:24 -0400

    can find alpha and beta angles but needs testing and bound checking

--- ariac_example/script/move_kitting.py
+++ ariac_example/script/move_kitting.py
@@ -3,11 +3,31 @@
 import rospy
 import tf2_ros
 import moveit_commander as mc
+import math
 
 import geometry_msgs.msg
 from tf.transformations import euler_from_quaternion
 
 import sys
+
+def find_alphabeta(x, z):
+	r1 = 0.573 # length of upper arm link (radius of its range of motion)
+	r2 = 0.400 # length of forearm link
+
+	ab = math.dist([-1.3, 1.127], [x,z]) # dist from kitting base joint to desired (x,z) point (a + b in proof)
+	beta = law_cosines_gamma(r1, r2, ab)
+
+	# alpha = alpha' + alpha'' (check proof)
+	a1 = law_cosines_gamma(r1, ab, r2)
+	a2 = math.acos((x+1.3)/ab)
+	alpha = a1 + a2
+
+	return (alpha, beta)
+
+# Uses law of cosines to find the angle (in radians) of the side opposite of c
+def law_cosines_gamma(a, b, c):
+	return math.acos((a**2 + b**2 - c**2)/(2*a*b))
+
 
 class MoveitRunner():
 	def __init__(self, group_names, node_name='move_kitting',
@@ -62,6 +82,12 @@ if __name__ == '__main__':
 
 	moveit_runner_kitting.groups['kitting_arm'].go(cur_joint_pose, wait=True)
 	moveit_runner_kitting.groups['kitting_arm'].stop()
+
+	# Finding alpha (shoulder lift angle) and beta (elbow joint angle)
+	alpha, beta = find_alphabeta(x, z)
+	
+
+
 
 
 
