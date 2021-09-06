7ae1f37e78160643ccde55eb1ab329eeb0b1b9e1
ariac <ariac@ubuntu1804.linuxvmimages.local>
2021-09-06 17:50:04 -0400

    new task: complete orders using controller

--- ariac_example/script/move_kitting.py
+++ ariac_example/script/move_kitting.py
@@ -10,6 +10,7 @@ from tf.transformations import euler_from_quaternion
 
 import sys
 
+
 def find_alphabeta(x, z):
 	r1 = 0.61215	# range of motion of shoulder lift joint
 	r2 = 0.57235	# range of motion of elbow joint
@@ -57,6 +58,64 @@ class MoveitRunner():
 			group.set_goal_tolerance(0.001)	# toggle this on and off
 			self.groups[group_name] = group
 
+	def goto_pose(self, x, y, z):
+
+		# TODO: incorporate roll pitch yaw in user input
+		orientation_k = kitting_arm.get_current_pose().pose.orientation
+		(roll, pitch, yaw) = euler_from_quaternion([orientation_k.x, orientation_k.y, orientation_k.z, orientation_k.w]) 
+
+		# Bounds checking
+		if y > 4.8 or y < -4.8:
+			return False
+		if not bounds_checking(x, z):
+			return False
+
+		conveyor_side = True if x >= -1.3 else False
+
+		# Finding alpha (shoulder lift angle) and beta (elbow joint angle)
+		if conveyor_side:
+			alpha, beta = find_alphabeta(x-0.1158, z+0.1)	# adjust these values to account for wrist lengths
+		else:
+			alpha, beta = find_alphabeta(x+0.1154, z+0.1)
+
+		print("alpha: ", alpha)
+		print("beta: ", beta)
+
+		cur_joint_pose = moveit_runner_kitting.groups['kitting_arm'].get_current_joint_values()
+
+		# linear arm actuator
+		if not conveyor_side:
+			cur_joint_pose[0] = y + 0.1616191
+		else:
+			cur_joint_pose[0] = y - 0.1616191
+
+		# shoulder pan joint
+		if x < -1.3:
+			cur_joint_pose[1] = 3.14
+		else:
+			cur_joint_pose[1] = 0
+
+		# shoudler lift (alpha) and elbow (beta)
+		cur_joint_pose[2] = alpha
+		cur_joint_pose[3] = beta
+
+		# to get flat ee: w1 = - shoulder lift - elbow - pi/2
+		cur_joint_pose[4] = -1*cur_joint_pose[2] - cur_joint_pose[3] - math.pi/2
+
+		# wrist 2 (always -pi/2 for now, until we incorporate roll, pitch, yaw)
+		cur_joint_pose[5] = -math.pi/2
+
+		# wrist 3
+		cur_joint_pose[6] = 0
+
+		print(cur_joint_pose)
+
+		self.groups['kitting_arm'].go(cur_joint_pose, wait=True)
+		self.groups['kitting_arm'].stop()		
+
+		# TODO: eventually want to check with tf frames if move was successful or not
+		return True
+
 if __name__ == '__main__':
 
 	kitting_group_names = ['kitting_arm']
@@ -65,9 +124,10 @@ if __name__ == '__main__':
 	kitting_arm = moveit_runner_kitting.groups['kitting_arm']
 	kitting_arm.set_end_effector_link("vacuum_gripper_link")
 
-	orientation_k = kitting_arm.get_current_pose().pose.orientation
-	(roll, pitch, yaw) = euler_from_quaternion([orientation_k.x, orientation_k.y, orientation_k.z, orientation_k.w])
+	move_success = moveit_runner_kitting.goto_pose(-1.15, 0, 2)
+	print("Move success: %s" % move_success)
 
+'''
 	# Get user input
 	x = input("Enter the desired x-value for the kitting robot: ")
 	valid_y = False
@@ -122,7 +182,7 @@ if __name__ == '__main__':
 	moveit_runner_kitting.groups['kitting_arm'].go(cur_joint_pose, wait=True)
 	moveit_runner_kitting.groups['kitting_arm'].stop()
 
-
+'''
 
 
 
