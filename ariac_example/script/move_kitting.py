fa269d3b0af7d5d959442ff136e5e11f21765d1c
Daniel Wu <dxwu2@illinois.edu>
2021-10-08 01:52:57 -0500

    reconfigured code, buggy buggy buggy

--- ariac_example/script/move_kitting.py
+++ ariac_example/script/move_kitting.py
@@ -4,12 +4,15 @@ import rospy
 import tf2_ros
 import moveit_commander as mc
 import math
+import yaml
+from yaml.loader import SafeLoader
 
 import geometry_msgs.msg
 from tf.transformations import euler_from_quaternion
 from nist_gear.msg import VacuumGripperState, Proximity
 from nist_gear.srv import VacuumGripperControl, ConveyorBeltControl
 from std_srvs.srv import Trigger
+from std_msgs.msg import String
 
 import sys
 
@@ -18,6 +21,17 @@ def start_competition():
 
     rospy.wait_for_service('/ariac/start_competition')
     rospy.ServiceProxy('/ariac/start_competition', Trigger)()
+
+def competition_state():
+	print('waiting')
+	data = rospy.wait_for_message('/ariac/competition_state', String)
+	print('got it')
+	print(data)
+	return data
+
+def move_agvs(agv, dest):
+	rospy.wait_for_service('/ariac/' + agv + '/to_' + dest)
+	rospy.ServiceProxy('/ariac/' + agv + '/to_' + dest, Trigger)()
 
 def get_breakbeam_sensor_data():
 	data = rospy.wait_for_message('/ariac/breakbeam_conveyor', Proximity)
@@ -164,21 +178,100 @@ class Follow_points():
 	def __init__(self, moveit_runner_kitting, gm):
 		self.moveit_runner_kitting = moveit_runner_kitting
 		self.gm = gm
-		self.tries = 0
+		self.state = 0
+		self.agvs = [0, 0, 0, 0]	# 0 = unused, 1 = used
+		with open("/home/dxwu2/araic_ws/src/ARIAC/nist_gear/config/user_config/sample_user_config.yaml", "r") as f:
+			data = yaml.load(f)
+			# breakbeam_conveyor_pose_xyz = data['sensors']['breakbeam_conveyor']['pose']['xyz']
+		self.sensor_file = data
 
 	def main_body(self):
-		if self.tries == 0:
-			self.loop_body()
-			self.tries = 1
+		# Start state: Kitting robot may be out of position
+		if self.state == 0:
+			start_pose = [-1.15, 0, 2]
+			self.moveit_runner_kitting.goto_pose(start_pose[0], start_pose[1], start_pose[2])
+			
+			# verify if there, then change state
+			cx = kitting_arm.get_current_pose().pose.position.x
+			cy = kitting_arm.get_current_pose().pose.position.y
+			cz = kitting_arm.get_current_pose().pose.position.z
+			if abs(cx - start_pose[0]) + abs(cy - start_pose[1]) + abs(cz - start_pose[2]) <= 0.01:
+				self.state = 1
 			return
-		if self.tries == 1:
-			self.loop_body()
-			self.tries = 2
+		
+		# State 1: Kitting robot in starting position
+		if self.state == 1:
+			if competition_state().data == "init":
+				start_competition()
+			else:
+				control_conveyor(100)
+			self.state = 2
 			return
-		if self.tries == 2:
-			self.loop_body()
-			self.tries = 3
+		
+		# State 2: Wait for battery
+		if self.state == 2:
+			while not (get_breakbeam_sensor_data().object_detected and get_breakbeam_flat_sensor_data().object_detected):
+				rospy.sleep(0.05)	# how to do this with pthread cond wait
+			control_conveyor(0)
+			self.state = 3
 			return
+
+		# State 3: Battery detected, moving arm toward battery
+		if self.state == 3:
+			self.gm.activate_gripper()
+			battery_detect_pose = self.sensor_file['sensors']['breakbeam_conveyor']['pose']['xyz']
+			battery_detect_pose[2] -= 0.985	# detection length is 1 meter (a little less for height of battery)
+			self.moveit_runner_kitting.goto_pose(battery_detect_pose[0], battery_detect_pose[1], battery_detect_pose[2])
+
+			# Verify robot moved there
+			cx = kitting_arm.get_current_pose().pose.position.x
+			cy = kitting_arm.get_current_pose().pose.position.y
+			cz = kitting_arm.get_current_pose().pose.position.z
+			print("cx:", cx - battery_detect_pose[0])
+			print("cy:", cy - battery_detect_pose[1])
+			print("cz:", cz - battery_detect_pose[2])
+			if abs(cx - battery_detect_pose[0]) + abs(cy - battery_detect_pose[1]) + abs(cz - battery_detect_pose[2]) <= 0.01:
+				self.state = 4
+			return
+		
+		# State 4: Lowering toward battery
+		if self.state == 4:
+			cx = kitting_arm.get_current_pose().pose.position.x
+			cy = kitting_arm.get_current_pose().pose.position.y
+			cz = kitting_arm.get_current_pose().pose.position.z
+			self.moveit_runner_kitting.goto_pose(cx, cy, cz - 0.002)
+			if self.gm.is_object_attached():
+				self.state = 5
+			return
+
+		# State 5: Moving to AGV
+		if self.state == 5:
+			agv2_pose = [-2.265, 1.3676, 1.0]
+			self.moveit_runner_kitting.goto_pose(-0.572989, 0, 2.0)	# waypoint to avoid crashing into conveyor
+			self.moveit_runner_kitting.goto_pose(agv2_pose[0], agv2_pose[1], agv2_pose[2])
+			
+			# verify if there, then change state
+			cx = kitting_arm.get_current_pose().pose.position.x
+			cy = kitting_arm.get_current_pose().pose.position.y
+			cz = kitting_arm.get_current_pose().pose.position.z
+			if abs(cx - agv2_pose[0]) + abs(cy - agv2_pose[1]) + abs(cz - agv2_pose[2]) <= 0.01:
+				self.state = 6
+			return
+
+		# State 6: Drop battery
+		if self.state == 6:
+			self.gm.deactivate_gripper()
+			rospy.sleep(2.0)
+			self.state = 7
+			return
+
+		# State 7: Transport to Gantry Robot
+		if self.state == 7:
+			move_agvs('agv2', 'as1')
+			self.state = 0
+			return
+
+
 
 	def loop_body(self):
 		# bandaid fix: get robot arm out of the way so no interference with break beam sensor
@@ -219,10 +312,11 @@ if __name__ == '__main__':
 	kitting_arm.set_end_effector_link("vacuum_gripper_link")
 	gm = GripperManager(ns='/ariac/kitting/arm/gripper/')
 
-	moveit_runner_kitting.goto_pose(-1.15, 0, 2)
+	# moveit_runner_kitting.goto_pose(-1.15, 0, 2)
 
-	control_conveyor(100)
+	# control_conveyor(100)
 
 	motionPlan = Follow_points(moveit_runner_kitting, gm)
-	while 1 > 0:
+	while True:
 		motionPlan.main_body()
+		print("state:", motionPlan.state)
