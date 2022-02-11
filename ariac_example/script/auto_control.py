137aef4be1ed5311c8386d95495140db10da29a5
Daniel Wu <dxwu2@illinois.edu>
2022-02-11 11:42:54 -0600

    classes added

--- ariac_example/script/auto_control.py
+++ ariac_example/script/auto_control.py
@@ -124,6 +124,143 @@ def law_cosines_gamma(a, b, c):
 def bounds_checking(x, z):
 	return True if euclidean_dist(x-0.1158, z+0.1, -1.3, 1.12725) <= 1.1845 else False
 
+def lvl2_consistency_check():
+	# Check if conveyor and kitting ranges of motion intersect
+
+	# Check if kitting and AGV ranges of motion intersect
+
+	# Check if AGV and gantry ranges of motion intersect
+
+
+def pick_place(moveit_runner_kitting, moveit_runner_gantry, kitting_gm, gantry_gm):
+	while True:
+		src = input("\nEnter pick (source) pose in form (x, y, z): ")
+		if len(src) == 3:
+			break
+		print("Input formatted incorrectly. Please try again")
+	
+	while True:
+		dst = input("\nEnter place (dest) pose in form (x, y, z): ")
+		if len(dst) == 3:
+			break
+		print("Input formatted incorrectly. Please try again")
+
+	# Check if pick-and-place operation is possible under the given constraints
+	if not reachable(kitting_base, src):
+		print("Pick (source) location is out of kitting robot's range.")
+		exit()
+	
+	if not reachable(kitting_base, dst):
+		print("Place (dest) location is out of kitting robot's range.")
+		exit()
+
+    # Boot up simulation (Gazebo) with the auto-generated controller
+	moveit_runner_kitting.goto_pose(src[0], src[1], src[2])
+	kitting_gm.activate_gripper()
+	moveit_runner_kitting.goto_pose(dst[0], dst[1], dst[2])
+	kitting_gm.deactivate_gripper()
+
+class KittingRobot:
+    def __init__(self, pose, orient, orient_range, max_r, id_count):
+        self.type = 'kitting'
+        self.pose = pose           # center pose - [x,y,z]
+        self.orient = orient   # direction in which rail runs: 0=x, 1=y
+        self.orient_range = orient_range   # range in its oriented direction (aka rail length)
+        self.max_r = max_r
+        self.shape = Cylinder(pose, orient, orient_range, max_r)
+        self.id = id_count
+        id_count += 1
+        
+    # Returns boolean indicating whether there exists intersection with a Gantry Robot
+    def intersect_w_gantry(self, GantryObject):
+        pass
+    
+    def intersect_w_conveyor(self, ConveyorObject):
+        # ASSUMPTION FOR NOW: kitting and conveyor lie in same direction
+        # check if oriented direction overlaps
+        if not overlap(self.orient_range, ConveyorObject.orient_range):
+            return false
+
+        # check if other two directions overlap (for now, if line lies inside cylinder -> if point inside circle)
+        return euclidean(self.shape.circle_center, ConveyorObject.shape.point_2d) < self.shape.r
+    
+    # FIX: agv should be represented as vertical line, not point
+    def intersect_w_agv(self, AGVObject):
+        # check if any of the AGV points are inside kitting cylinder
+        for loc in AGVObject.dst:
+            # if AGV orient dimension falls out of range of cylinder orient range, continue
+            if loc[AGVObject.orient] < self.shape.orient_range[0] or loc[AGVObject.orient] > self.shape.orient_range[1]:
+                continue
+
+            # check other two dimensions
+            if euclidean(self.shape.circle_center, get_2d_from_3d(loc,self.orient)) < self.max_r:
+                return True
+
+        return False
+
+class GantryRobot:
+    def __init__(self, pose, x_rail_range, y_rail_range, arm_r, id_count):
+        self.type = 'gantry'
+        self.pose = pose
+        self.x_rail_range = x_rail_range   # [x_min, x_max]
+        self.y_rail_range = y_rail_range   # [y_min, y_max]
+        self.arm_r = arm_r
+        self.id = id_count
+        id_count += 1
+        
+class AGVRobot:
+    def __init__(self, pose, orient, dst, id_count):
+        self.type = 'agv'
+        self.pose = pose   # starting pose of AGV
+        self.orient = orient   # 0=x, 1=y
+        self.shape = []   # list of vertical lines
+        for i in dst:
+            self.shape.append(Line(pose, 2, [0.81,2]))   # TODO: z-range hardcoded for now
+        self.dst = dst   # list of possible locations (inc. start) (change if can control AGV) -> [[x1,y1,z1],[x2,y2,z2],...]
+        # FIX THIS: SHOULD NOT BE A POINT, SHOULD BE A VERTICAL LINE
+        
+        self.id = id_count
+        id_count += 1
+        
+class ConveyorRobot:
+    def __init__(self, pose, orient, orient_range, dim, id_count):
+        self.type = 'conveyor'
+        self.pose = pose   # center pose [x,y,z]
+        self.orient = orient   # 0 = runs along x-direction, 1 = runs along y-direction
+        self.orient_range = orient_range
+        self.dim = dim   # [x length, y length, z height]
+        # compute line for this (see intersect_kitting_conveyor for formatting)
+        self.shape = Line([pose[0],pose[1],dim[2]], orient, orient_range)
+        self.id = id_count
+        id_count += 1
+
+class Line:
+    def __init__(self, pose, orient, orient_range):
+        self.orient = orient
+        self.orient_range = orient_range
+        self.point_2d = self.get_point(pose)
+    
+    def get_point(self, x):
+        point = []
+        for dim in range(3):
+            if dim != self.orient:
+                point.append(x[dim])
+        return point
+
+class Cylinder:
+    def __init__(self, pose, orient, orient_range, max_r):
+        self.orient = orient
+        self.orient_range = orient_range
+        self.circle_center = self.get_point(pose)
+        self.r = max_r
+    
+    def get_point(self, x):
+        point = []
+        for dim in range(3):
+            if dim != self.orient:
+                point.append(x[dim])
+        return point
+
 
 class MoveitRunner():
 	def __init__(self, group_names, node_name='move_kitting',
@@ -254,31 +391,8 @@ if __name__ == '__main__':
 	global max_radius
 	max_radius = upper_arm_length + forearm_length
 
+	# Level 2 Consistency Check - connectivity (for now, assuming conveyor -> kitting -> AGV -> gantry)
+
 	# Pick-and-place operation
-	while True:
-		src = input("\nEnter pick (source) pose in form (x, y, z): ")
-		if len(src) == 3:
-			break
-		print("Input formatted incorrectly. Please try again")
-	
-	while True:
-		dst = input("\nEnter place (dest) pose in form (x, y, z): ")
-		if len(dst) == 3:
-			break
-		print("Input formatted incorrectly. Please try again")
-
-	# Check if pick-and-place operation is possible under the given constraints
-	if not reachable(kitting_base, src):
-		print("Pick (source) location is out of kitting robot's range.")
-		exit()
-	
-	if not reachable(kitting_base, dst):
-		print("Place (dest) location is out of kitting robot's range.")
-		exit()
-
-    # Boot up simulation (Gazebo) with the auto-generated controller
-	moveit_runner_kitting.goto_pose(src[0], src[1], src[2])
-	kitting_gm.activate_gripper()
-	moveit_runner_kitting.goto_pose(dst[0], dst[1], dst[2])
-	kitting_gm.deactivate_gripper()
+	# make call to function
     
